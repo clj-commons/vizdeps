34dff03f722db380b013395ec51b5b4479a26723
danielcompton <desk@danielcompton.net>
2018-12-16 19:46:35 +1300

    Version 0.1.7-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject walmartlabs/vizdeps "0.1.7"
+(defproject walmartlabs/vizdeps "0.1.7-SNAPSHOT"
   :description "Visualize Leiningen project dependencies using Graphviz."
   :url "https://github.com/walmartlabs/vizdeps"
   :license {:name "Apache Sofware License 2.0"
